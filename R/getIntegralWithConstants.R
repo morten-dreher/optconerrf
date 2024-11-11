ea7b9784c58934788a5086dea103a62e1185b38c
morten-dreher <morten.dreher@outlook.de>
2024-11-11 13:17:02 +0100

    Package updates
    Updated documentation, removed unnecessary function getInnerPsiConstant()

--- R/getIntegralWithConstants.R
+++ R/getIntegralWithConstants.R
@@ -1,26 +1,32 @@
 # TODO: extend to more than one interval
 #' Calculate the integral over a partially constant function
 #'
+#' @description This function aims to provide a more accurate integration routine than the \code{integrate()} function
+#' for a partially constant function.
+#'
 #' @template param_constant_integrate
 #' @template param_design
 #'
 getIntegralWithConstants <- function(constant, design) {
+  # Integrate over the first (non-constant) part of the function
   part1 <- stats::integrate(
     f = getInnerPsi, lower = design$alpha1, upper = design$monotonisationConstants$dls[1],
     constant = constant, design = design)$value
 
-  # Manually calculate integral over constant part of function
+  # Manually calculate integral over the constant part of function
   # The first-stage p-value to be used is slightly shifted from the lower bound
+  # (the shifting might not be necessary)
   part2 <- min(max(getPsi(nuPrime = -exp(constant)/getQ(
     firstStagePValue = design$monotonisationConstants$dls[1]+1e-6,
     design = design), conditionalPower = design$conditionalPower), design$minimumConditionalError),
     design$maximumConditionalError)*
     (design$monotonisationConstants$dus[1] - design$monotonisationConstants$dls[1])
 
-
+  # Integrate over the final (non-constant) part of the function
   part3 <- stats::integrate(
     f = getInnerPsi, lower = design$monotonisationConstants$dus[1], upper = design$alpha0,
     constant = constant, design = design)$value
 
+  # Return the sum of the individual integrals
   return(sum(c(part1, part2, part3)))
 }
