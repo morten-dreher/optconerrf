6302ab8bf9841ffc9de781e430b31d175273b0ea
morten-dreher <morten.dreher@outlook.de>
2024-12-04 09:12:58 +0100

    Added tests for design object and level constant

--- tests/testthat/test-getDesignOptimalConditionalErrorFunction.R
+++ tests/testthat/test-getDesignOptimalConditionalErrorFunction.R
@@ -1,4 +1,4 @@
-test_that("Design objects correctly created", {
+test_that("Class objects correctly created", {
   design_interim <- getDesignOptimalConditionalErrorFunction(
     alpha = 0.025, alpha1 = 0.001, alpha0 = 0.5,
     conditionalPower = 0.9, delta1 = 0.5, useInterimEstimate = TRUE,
@@ -7,11 +7,34 @@ test_that("Design objects correctly created", {
   )
   expect_s4_class(design_interim, "TrialDesignOptimalConditionalError")
 
+  expect_equal(design_interim$alpha, 0.025)
+  expect_equal(design_interim$alpha1, 0.001)
+  expect_equal(design_interim$alpha0, 0.5)
+  expect_equal(design_interim$conditionalPower, 0.9)
+  expect_equal(design_interim$delta1, 0.5)
+  expect_equal(design_interim$useInterimEstimate, TRUE)
+  expect_equal(design_interim$firstStageInformation, 4)
+  expect_equal(design_interim$likelihoodRatioDistribution, "maxlr")
+  expect_equal(design_interim$enforceMonotonicity, TRUE)
+  expect_equal(design_interim$ncp1, 0.5*sqrt(4), tolerance = 1e-8)
+
   design_fixed <- getDesignOptimalConditionalErrorFunction(
     alpha = 0.025, alpha1 = 0.001, alpha0 = 0.5,
     conditionalPower = 0.9, delta1 = 0.5, useInterimEstimate = FALSE,
     firstStageInformation = 4, likelihoodRatioDistribution = "fixed",
     deltaLR = 1, enforceMonotonicity = FALSE
   )
-  expect_s4_class(design_interim, "TrialDesignOptimalConditionalError")
+  expect_s4_class(design_fixed, "TrialDesignOptimalConditionalError")
+
+  expect_equal(design_fixed$alpha, 0.025)
+  expect_equal(design_fixed$alpha1, 0.001)
+  expect_equal(design_fixed$alpha0, 0.5)
+  expect_equal(design_fixed$conditionalPower, 0.9)
+  expect_equal(design_fixed$delta1, 0.5)
+  expect_equal(design_fixed$useInterimEstimate, FALSE)
+  expect_equal(design_fixed$firstStageInformation, 4)
+  expect_equal(design_fixed$likelihoodRatioDistribution, "fixed")
+  expect_equal(design_fixed$deltaLR, 1)
+  expect_equal(design_fixed$enforceMonotonicity, FALSE)
+  expect_equal(design_fixed$ncp1, 0.5*sqrt(4), tolerance = 1e-8)
 })
