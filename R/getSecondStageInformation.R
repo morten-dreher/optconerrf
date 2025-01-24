29fbcf993a5c7ddc6ba8d4ecda0adc7e5c8f3b34
morten-dreher <morten.dreher@outlook.de>
2025-01-24 09:33:55 +0100

    Second-stage information now calculated from p1
    Changed function behaviour from using conditional errors to using first-stage p-values

--- R/getSecondStageInformation.R
+++ R/getSecondStageInformation.R
@@ -1,35 +1,67 @@
 #' Calculate the Second-stage Information
 #' @name getSecondStageInformation
 #'
-#' @description Calculate second-stage information for given alpha, conditional power and effect size
+#' @description Calculate second-stage information for given first-stage p-value and design.
 #'
 #' @details The second-stage information \eqn{I_{2}} is calculated given a first-stage p-value \eqn{p_1} as:
 #' \deqn{I_{2}(p_1) = \frac{(\Phi^{-1}(1-\alpha_2(p_1)) + \Phi^{-1}(CP))^2}{\Delta_1^2} = \frac{\nu(\alpha_2(p_1))}{\Delta_1^2},}
 #' where
 #' \itemize{
-#'    \item \eqn{\alpha_2(p_1)} is the second-stage significance level (e.g., a conditional error)
+#'    \item \eqn{\alpha_2(p_1)} is the conditional error function
 #'    \item \eqn{CP} is the target conditional power
 #'    \item \eqn{\Delta_1} is the assumed treatment effect (expressed as a mean difference).
 #' }
+#' The conditional error is calculated according to the specification provided in the \code{design} argument.
+#' For p-values smaller or equal to the first-stage efficacy boundary as well as p-values greater than the first-stage futility boundary,
+#' the returned information is 0.
 #'
-#' @param conditionalError Conditional error rate of the design.
-#' @template param_conditionalPower
-#' @param delta1 The effect size to power for.
+#' @template param_firstStagePValue
+#' @template param_design
 #'
 #' @return The second-stage information.
 #' @export
 #'
 #' @examples
-#' getSecondStageInformation(
-#' conditionalError = 0.05, conditionalPower = 0.9, delta1 = 0.25)
+#' design <- getDesignOptimalConditionalErrorFunction(
+#'   alpha = 0.025, alpha1 = 0.001, alpha0 = 0.5,
+#'   conditionalPower = 0.9, delta1 = 0.25, useInterimEstimate = FALSE,
+#'   firstStageInformation = 4, likelihoodRatioDistribution = "maxlr"
+#' )
 #'
+#' getSecondStageInformation(
+#'   firstStagePValue = c(0.05, 0.1, 0.2), design = design
+#' )
 #'
 #' @references Brannath, W. & Bauer, P. (2004). Optimal conditional error functions for the control of conditional power. Biometrics, 60 (3), 715–723. https://doi.org/10.1111/j.0006-341X.2004.00221.x
 #'
-getSecondStageInformation <- function(conditionalError, conditionalPower, delta1) {
-  return((getNu(alpha = conditionalError, conditionalPower = conditionalPower))
-         /(delta1^2))
+getSecondStageInformation <- function(firstStagePValue, design) {
+  # For p-values outside of the continuation region, return information 0
+  if (firstStagePValue <= design$alpha1 || firstStagePValue > design$alpha0) {
+    information <- 0
+  } else {
+    # For design with interim estimate, apply effect restrictions
+    if (design$useInterimEstimate) {
+      effect <- min(
+        max(qnorm(1 - firstStagePValue) / design$firstStageInformation, design$delta1Min),
+        design$delta1Max
+      )
+    }
+    # For design without interim estimate, use fixed effect
+    else {
+      effect <- design$delta1
+    }
+
+    # Calculate conditional error
+    conditionalError <- getOptimalConditionalError(
+      firstStagePValue = firstStagePValue, design = design
+    )
+
+    information <- (getNu(alpha = conditionalError, conditionalPower = design$conditionalPower)) / (effect^2)
+  }
+  return(information)
 }
 
-getSecondStageInformation <- Vectorize(FUN = getSecondStageInformation,
-                                      vectorize.args = c("conditionalError", "conditionalPower", "delta1"))
+getSecondStageInformation <- Vectorize(
+  FUN = getSecondStageInformation,
+  vectorize.args = c("firstStagePValue")
+)
