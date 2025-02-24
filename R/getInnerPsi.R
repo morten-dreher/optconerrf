28e3817fc55f15253184792ac0cf890b331ca963
morten-dreher <morten.dreher@outlook.de>
2025-02-24 14:46:47 +0100

    Constraints can now be data-dependent and are applied in getInnerPsi
    Same code as in getOptimalConditionalError()

--- R/getInnerPsi.R
+++ R/getInnerPsi.R
@@ -22,16 +22,42 @@ getInnerPsi <- function(firstStagePValue, constant, design) {
     Q <- getQ(firstStagePValue = firstStagePValue, design = design)
   }
 
+  # Calculate the value to be supplied to getPsi
   inner <- -exp(constant)/Q
 
+  # Identify conditional power to be supplied to getPsi and to be used for (information) constraints
   # Check if conditional power function should be used
   if(!is.null(suppressWarnings(body(design$conditionalPowerFunction)))) {
     conditionalPower <- design$conditionalPowerFunction(firstStagePValue)
   }
+  # Constant conditional power
   else {
     conditionalPower <- design$conditionalPower
   }
 
+  # Calculate possibly data-dependent constraints
+  # Identify effect for constraints
+  if(design$useInterimEstimate) {
+    delta1 <- pmin(pmax(qnorm(1-firstStagePValue)/sqrt(design$firstStageInformation), design$delta1Min), design$delta1Max)
+  }
+  else {
+    delta1 <- design$delta1
+  }
+
+  # Constraints on conditional error scale
+  C_max <- design$maximumConditionalError
+  C_min <- design$minimumConditionalError
+
+  #If minimumSecondStageInformation is given, use this instead of maximumConditionalError
+  if(design$minimumSecondStageInformation > 0){
+    C_max <- 1 - pnorm(delta1* sqrt(design$minimumSecondStageInformation)-qnorm(conditionalPower))
+  }
+
+  #If maximumSecondStageInformation is given, use this instead of minimumConditionalError
+  if(design$maximumSecondStageInformation < Inf){
+    C_min <- 1 - pnorm(delta1* sqrt(design$maximumSecondStageInformation)-qnorm(conditionalPower))
+  }
+
   return(pmin(pmax(getPsi(nuPrime = inner, conditionalPower = conditionalPower),
-                   design$minimumConditionalError), design$maximumConditionalError))
+                   C_min), C_max))
 }
