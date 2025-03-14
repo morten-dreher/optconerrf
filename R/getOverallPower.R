b04c3cede4b5a838d1595931dca8f77c145ffe34
morten-dreher <morten.dreher@outlook.de>
2025-03-14 08:54:37 +0100

    Small changes in variable naming for consistency with other functions

--- R/getOverallPower.R
+++ R/getOverallPower.R
@@ -4,21 +4,23 @@
 #' @description Calculate the overall power
 #'
 #' @template param_design
+#' @template param_delta1_overallPower
 #'
+#' @return The overall power of the design at the provided effect size.
 #'
 
-getOverallPower <- function(design, delta){
+getOverallPower <- function(design, delta1) {
+  ncp1 <- delta1 * sqrt(design$firstStageInformation)
 
-  ncp <- delta*sqrt(design$firstStageInformation)
-
-  if (!is.na(design$conditionalPower)){
-    power <- 1 - pnorm(qnorm(1-design$alpha1)-ncp)+design$conditionalPower*(pnorm(qnorm(1-design$alpha1)-ncp)-pnorm(qnorm(1-design$alpha0)-ncp))
-  }else{
-    integrate <- function(firstStagePValue){design$conditionalPowerFunction(firstStagePValue)*exp(qnorm(1-firstStagePValue)*ncp-ncp^2/2)}
-    integral <- stats::integrate(f = integrate, lower = design$alpha1, upper = design$alpha0)$value
-    power <- 1 - pnorm(qnorm(1-design$alpha1)-ncp)+integral
+  if (!is.na(design$conditionalPower)) {
+    power <- 1 - pnorm(qnorm(1 - design$alpha1) - ncp1) + design$conditionalPower * (pnorm(qnorm(1 - design$alpha1) - ncp1) - pnorm(qnorm(1 - design$alpha0) - ncp1))
+  } else {
+    secondStageRejection <- function(firstStagePValue) {
+      design$conditionalPowerFunction(firstStagePValue) * exp(qnorm(1 - firstStagePValue) * ncp1 - ncp1^2 / 2)
+    }
+    integral <- stats::integrate(f = secondStageRejection, lower = design$alpha1, upper = design$alpha0)$value
+    power <- 1 - pnorm(qnorm(1 - design$alpha1) - ncp1) + integral
   }
 
   return(power)
 }
-
