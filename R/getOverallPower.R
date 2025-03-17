5b245806d3700bf9c197b2b71894f62b92827b55
morten-dreher <morten.dreher@outlook.de>
2025-03-17 12:33:47 +0100

    Adjusted argument and variable naming to package convention

--- R/getOverallPower.R
+++ R/getOverallPower.R
@@ -9,19 +9,19 @@
 #' @return The overall power of the design at the provided effect size.
 #'
 
-getOverallPower <- function(design, delta) {
+getOverallPower <- function(design, delta1) {
 
-  ncp <- delta * sqrt(design$firstStageInformation)
+  ncp1 <- delta1 * sqrt(design$firstStageInformation)
 
   # Calculate probability to reject at the second stage for given delta
   secondStageRejection <- function(firstStagePValue) {
-    (1-pnorm(qnorm(1 - getOptimalConditionalError(firstStagePValue, design = design))- sqrt(getSecondStageInformation(firstStagePValue, design = design)) * delta)) *
-    exp(qnorm(1 - firstStagePValue) * ncp - ncp^2 / 2)
+    (1-pnorm(qnorm(1 - getOptimalConditionalError(firstStagePValue, design = design))- sqrt(getSecondStageInformation(firstStagePValue, design = design)) * delta1)) *
+    exp(qnorm(1 - firstStagePValue) * ncp1 - ncp1^2 / 2)
   }
 
   integral <- stats::integrate(f = secondStageRejection, lower = design$alpha1, upper = design$alpha0)$value
 
-  OverallPower <- 1 - pnorm(qnorm(1 - design$alpha1) - ncp) + integral
+  overallPower <- 1 - pnorm(qnorm(1 - design$alpha1) - ncp1) + integral
 
-  return(OverallPower)
+  return(overallPower)
 }
