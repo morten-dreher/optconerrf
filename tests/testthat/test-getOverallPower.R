aa66e35e677304510448e4dc7cb440415c2913c5
morten-dreher <morten.dreher@outlook.de>
2025-03-17 12:36:27 +0100

    Now skipping most tests on CRAN for runtime reasons

--- tests/testthat/test-getOverallPower.R
+++ tests/testthat/test-getOverallPower.R
@@ -1,6 +1,6 @@
 test_that("Overall power correctly calculated", {
 
-#Test if the calculated overall power matches with two special cases (fix cp, fixed LR, without use of Interim Estimate)
+# Test if the calculated overall power matches with two special cases (fix cp, fixed LR, without use of Interim Estimate)
 design_fix <- getDesignOptimalConditionalErrorFunction(
     alpha = 0.025, alpha1 = 0.001, alpha0 = 0.5, conditionalPower = 0.9,
     delta1 = 0.5, likelihoodRatioDistribution = "fixed", deltaLR = 1,
@@ -11,9 +11,13 @@ power <- 1 - pnorm(qnorm(1 - design_fix$alpha1) - ncp) +
   design_fix$conditionalPower * (pnorm(qnorm(1 - design_fix$alpha1) - ncp) - pnorm(qnorm(1 - design_fix$alpha0) - ncp))
 
 expect_equal(getOverallPower(design_fix, design_fix$delta1), power, tolerance = 1e-4)
+
+# Skip remaining tests on CRAN
+skip_on_cran()
+
 expect_equal(getOverallPower(design_fix, 0), design_fix$alpha, tolerance = 1e-4)
 
-#Test if the calculated overall power matches with two special cases (cp function, fixed LR, without use of Interim Estimate)
+# Test if the calculated overall power matches with two special cases (cp function, fixed LR, without use of Interim Estimate)
 design_function <- getDesignOptimalConditionalErrorFunction(
   alpha = 0.05, alpha1 = 0.001, alpha0 = 0.5, conditionalPowerFunction = function(x) pnorm(1-x),
   delta1 = 0.7, likelihoodRatioDistribution = "fixed", deltaLR = 1,
