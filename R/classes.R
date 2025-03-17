ccd31b5a458bbd6fd6ee9d3e4848dd31dd2f2bfe
morten-dreher <morten.dreher@outlook.de>
2025-03-17 12:38:42 +0100

    Improved error handling and range checks. Minor changes to function behaviour.
    For fixed effect, delta1 is now preferred and ncp1 ignored if both delta1 and ncp1 are specified (same behaviour as with interim estimate). If both CP value and CP function are specified, CP value is preferred and CP function ignored.

--- R/classes.R
+++ R/classes.R
@@ -70,18 +70,27 @@ TrialDesignOptimalConditionalError <- setRefClass(
     ) {
 
       # Range checks for numeric variables
+      # General range checks
       rangeCheck(variable = alpha, range = c(0, 1), allowedEqual = FALSE)
+      rangeCheck(variable = alpha1, range = c(0, 1), allowedEqual = TRUE)
+      rangeCheck(variable = alpha0, range = c(0, 1), allowedEqual = TRUE)
+
+      # Context-related range checks
       rangeCheck(variable = alpha1, range = c(0, alpha), allowedEqual = TRUE)
       rangeCheck(variable = alpha0, range = c(alpha1, 1), allowedEqual = TRUE)
 
       if(is.na(conditionalPower) && is.null(suppressWarnings(body(conditionalPowerFunction)))) {
-        stop("Must specify either conditionalPower or conditionalPowerFunction")
+        stop("Must specify either conditionalPower or a valid conditionalPowerFunction.")
       }
       else {
         if(!is.na(conditionalPower)) {
           rangeCheck(variable = conditionalPower, range = c(0, 1), allowedEqual = FALSE)
+
+          if(!is.null(suppressWarnings(body(conditionalPowerFunction)))) {
+            warning("Both conditionalPower and conditionalPowerFunction are provided. Using conditionalPower and ignoring conditionalPowerFunction.")
+          }
         }
-        if(!is.null(suppressWarnings(body(conditionalPowerFunction)))) {
+        else if(!is.null(suppressWarnings(body(conditionalPowerFunction)))) {
 
           # Check if function is increasing
           # Grid of values
@@ -102,8 +111,6 @@ TrialDesignOptimalConditionalError <- setRefClass(
       }
 
       rangeCheck(variable = firstStageInformation, range = c(0, Inf), allowedEqual = FALSE)
-      rangeCheck(variable = minimumConditionalError, range = c(0, 1), allowedEqual = TRUE)
-      rangeCheck(variable = maximumConditionalError, range = c(0, 1), allowedEqual = TRUE)
 
       # Set initial parameters
       .self$alpha <- alpha
@@ -113,6 +120,11 @@ TrialDesignOptimalConditionalError <- setRefClass(
       .self$firstStageInformation <- firstStageInformation
       .self$likelihoodRatioDistribution <- likelihoodRatioDistribution
       .self$useInterimEstimate <- useInterimEstimate
+
+      if(levelConstantMinimum >= levelConstantMaximum) {
+        stop("levelConstantMinimum must be smaller than levelConstantMaximum.")
+      }
+
       .self$levelConstantMinimum <- levelConstantMinimum
       .self$levelConstantMaximum <- levelConstantMaximum
 
@@ -124,13 +136,19 @@ TrialDesignOptimalConditionalError <- setRefClass(
           stop("Must provide a lower limit for the interim estimate by using ncp1Min or delta1Min.")
         }
         else if(!is.na(delta1Min)) {
+          rangeCheck(variable = delta1Min, range = c(0, Inf), allowedEqual = FALSE)
           .self$delta1Min <- delta1Min
           .self$delta1Max <- delta1Max
+
+          if(!is.na(ncp1Min)) {
+            warning("Both ncp1Min and delta1Min are provided. Using delta1Min and ignoring ncp1Min.")
+          }
 
           .self$ncp1Min <- delta1Min * sqrt(firstStageInformation)
           .self$ncp1Max <- delta1Max * sqrt(firstStageInformation)
         }
         else if(!is.na(ncp1Min)) {
+          rangeCheck(variable = ncp1Min, range = c(0, Inf), allowedEqual = FALSE)
           .self$ncp1Min <- ncp1Min
           .self$ncp1Max <- ncp1Max
 
@@ -145,22 +163,50 @@ TrialDesignOptimalConditionalError <- setRefClass(
       # When not using an interim estimate, derive fixed effects
       else {
         # If non-centrality parameter was not specified, calculate it from delta1
-        if(is.na(ncp1) && !is.na(delta1)) {
+        if(!is.na(delta1)) {
           rangeCheck(variable = delta1, range = c(0, Inf), allowedEqual = FALSE)
           .self$delta1 <- delta1
+
+          if(!is.na(ncp1)) {
+            warning("Both delta1 and ncp1 are provided. Using delta1 and ignoring ncp1.")
+          }
           .self$ncp1 <- delta1*sqrt(firstStageInformation)
         }
         # If delta1 was not specified, calculate it from ncp1
-        else if(is.na(delta1) && !is.na(ncp1)) {
+        else if(!is.na(ncp1)) {
           rangeCheck(variable = ncp1, range = c(0, Inf), allowedEqual = FALSE)
           .self$ncp1 <- ncp1
           .self$delta1 <- ncp1/sqrt(firstStageInformation)
         }
-        # Else, either none of or both of ncp1 and delta1 were specified
+        # Else, none of ncp1 and delta1 were specified
         else {
-          stop("Must specify exactly one of delta1 and ncp1 when using a fixed effect for conditional power.")
+          stop("Must specify delta1 or ncp1 when using a fixed effect for conditional power.")
         }
       }
+
+      # Range checks for constraints
+      # General range checks
+      rangeCheck(variable = minimumConditionalError, range = c(0, 1), allowedEqual = TRUE)
+      rangeCheck(variable = maximumConditionalError, range = c(0, 1), allowedEqual = TRUE)
+
+      rangeCheck(variable = minimumSecondStageInformation, range = c(0, Inf), allowedEqual = TRUE)
+      rangeCheck(variable = maximumSecondStageInformation, range = c(0, Inf), allowedEqual = TRUE)
+
+      if(maximumSecondStageInformation == 0) {
+        stop("Maximum second-stage information must be larger than 0.")
+      }
+
+      # Context-related range checks
+      rangeCheck(variable = minimumConditionalError, range = c(0, maximumConditionalError), allowedEqual = TRUE,
+                 hint = "It must not exceed maximumConditionalError.")
+      rangeCheck(variable = maximumConditionalError, range = c(minimumConditionalError, 1), allowedEqual = TRUE,
+                 hint = "It must not be smaller than minimumConditionalError.")
+
+      rangeCheck(variable = minimumSecondStageInformation, range = c(0, maximumSecondStageInformation),
+                 allowedEqual = TRUE, hint = "It must not exceed maximumSecondStageInformation.")
+      rangeCheck(variable = maximumSecondStageInformation, range = c(minimumSecondStageInformation, Inf),
+                 allowedEqual = TRUE, hint = "It must not be smaller than minimumSecondStageInformation.")
+
 
       # Identify constraints for minimum conditional error / maximum second-stage information
       .self$minimumConditionalError <- minimumConditionalError
@@ -175,21 +221,35 @@ TrialDesignOptimalConditionalError <- setRefClass(
       # Identify specific distribution parameters
       if(likelihoodRatioDistribution == "fixed") {
         if(any(is.na(deltaLR))) {
-          stop("Must provide deltaLR for fixed effect in likelihood ratio")
+          stop("Must provide deltaLR for fixed effect in likelihood ratio.")
         }
         else {
           .self$deltaLR <- deltaLR
+          # If any of the weights are NA, use equal weights
           if(any(is.na(weightsDeltaLR))) {
             .self$weightsDeltaLR <- rep(1/length(deltaLR), length(deltaLR))
+            # For multiple effects, tell the user that equal weights are used.
+            if(length(deltaLR) > 1) {
+              message("At least one entry in weightsDeltaLR is NA. Using equal weights for effects in fixed likelihood ratio.")
+            }
           }
           else {
+            rangeCheck(variable = weightsDeltaLR, range = c(0, 1), allowedEqual = TRUE)
+            # Check if weightsDeltaLR and deltaLR are of equal length
+            if(length(weightsDeltaLR) != length(deltaLR)) {
+              stop("Must provide exactly one weight in weightsDeltaLR per entry of deltaLR.")
+            }
+            # Verify that weightsDeltaLR sums to 1
+            if(sum(weightsDeltaLR) != 1) {
+              stop("Weights in weightsDeltaLR must sum to 1.")
+            }
             .self$weightsDeltaLR <- weightsDeltaLR
           }
         }
       }
       else if(likelihoodRatioDistribution == "normal") {
         if(is.na(deltaLR) || is.na(tauLR)) {
-          stop("Must provide deltaLR and tauLR for normal prior in likelihood ratio")
+          stop("Must provide deltaLR and tauLR for normal prior in likelihood ratio.")
         }
         else {
 
@@ -201,7 +261,7 @@ TrialDesignOptimalConditionalError <- setRefClass(
       }
       else if(likelihoodRatioDistribution == "exp") {
         if(is.na(kappaLR)) {
-          stop("Must provide kappaLR for exponential prior in likelihood ratio")
+          stop("Must provide kappaLR for exponential prior in likelihood ratio.")
         }
         else {
           rangeCheck(variable = kappaLR, range = c(0, Inf), allowedEqual = FALSE)
@@ -210,7 +270,7 @@ TrialDesignOptimalConditionalError <- setRefClass(
       }
       else if(likelihoodRatioDistribution == "unif") {
         if(is.na(deltaMaxLR)) {
-          stop("Must provide deltaMaxLR for uniform prior in likelihood ratio")
+          stop("Must provide deltaMaxLR for uniform prior in likelihood ratio.")
         }
         else {
           rangeCheck(variable = deltaMaxLR, range = c(0, Inf), allowedEqual = FALSE)
@@ -221,7 +281,7 @@ TrialDesignOptimalConditionalError <- setRefClass(
 
       }
       else {
-        stop("Distribution not matched.")
+        stop("Distribution not matched. likelihoodRatioDistribution should be one of 'fixed', 'normal', 'exp', 'unif' or 'maxlr'.")
       }
 
       # Calculate monotonisation constants
