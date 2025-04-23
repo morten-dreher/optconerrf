a5a18ea9d69d8db1d97aef8bc3c712f0d8ce7bd8
morten-dreher <morten.dreher@outlook.de>
2025-04-23 09:51:22 +0200

    Corrected constraints description
    Now adapted to the fact that constraints can be applied on both scales simultaneously

--- man-roxygen/param_minimumConditionalError.R
+++ man-roxygen/param_minimumConditionalError.R
@@ -1,1 +1,1 @@
-#' @param minimumConditionalError Lower boundary for the optimal conditional error function. Default 0 (no restriction). Is superseded by restrictions provided by \code{maximumSecondStageInformation}.
+#' @param minimumConditionalError Lower boundary for the optimal conditional error function. Default 0 (no restriction).
